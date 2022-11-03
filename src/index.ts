de1b40ae2dd8edb804aa81d7ab87da6eef49ac29
Dipper30 <ddddipper30@gmail.com>
2022-11-03 18:42:09 -0400

    v1.1.0: export a factory function for Validator (#4)
    * feature: export factory function
    
    * feature: new version

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,15 @@ import Utils from './Utils'
 
 const override = Utils.resetFunction.bind(Utils)
 
+/**
+ * a factory function which creates a new Validator
+ * @param {any} data optional
+ * @returns a new Validator
+ */
+const validate = (data?: any) => new Validator(data)
+
 export {
   Validator,
+  validate,
   override,
 }
